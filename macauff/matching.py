25cb37d9205ff6fc24863ae47ba72617d5133d79
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-07-06 11:18:19 +0100

    Initialisation of CrossMatch class, with docstring

---
+++ macauff/matching.py
@@ -0,0 +1,22 @@
+# Licensed under a 3-clause BSD style license - see LICENSE
+'''
+This module provides the high-level framework for performing catalogue-catalogue cross-matches.
+'''
+
+__all__ = ['CrossMatch']
+
+
+class CrossMatch():
+    '''
+    A class to cross-match two photometric catalogues with one another, producing
+    a composite catalogue of merged sources.
+
+    The class takes a path, the location of a metadata file containing all of the
+    necessary parameters for the cross-match, and outputs a file containing the
+    appropriate columns of the datasets plus additional derived parameters.
+
+    Parameters
+    ----------
+    file_path : string
+        A path to the location of the file containing the cross-match metadata.
+    '''
