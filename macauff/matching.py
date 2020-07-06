f3396f629edc63769a3b803b04822e3aed860651
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-07-06 13:52:17 +0100

    Read run flags from metadata file, and check for various combinations/permutations of acceptable values

--- macauff/matching.py
+++ macauff/matching.py
@@ -4,6 +4,8 @@ This module provides the high-level framework for performing catalogue-catalogue
 '''
 
 import os
+from configparser import ConfigParser
+import numpy as np
 
 __all__ = ['CrossMatch']
 
@@ -24,6 +26,54 @@ class CrossMatch():
     '''
 
     def __init__(self, file_path):
+        '''
+        Initialisation function for cross-match class.
+        '''
         if not os.path.isfile(file_path):
-            raise FileNotFoundError("Metadata file could not be found at "
-                                    "specified location.")
+            raise FileNotFoundError("Metadata file could not be found at specified location.")
+
+        self.file_path = file_path
+
+        self.read_metadata()
+
+        # Important steps that can be save points in the match process are:
+        # AUF creation, island splitting, c/f creation, star pairing. We have
+        # to check if any later stages are flagged to not run (i.e., they are
+        # the starting point) than earlier stages, and raise an error.
+        flags = np.array([self.run_auf, self.run_group, self.run_cf, self.run_star])
+        for i in range(3):
+            if flags[i] and np.any(~flags[i+1:]):
+                raise ValueError("Inconsistency between run/no run flags; please ensure that "
+                                 "if a sub-process is set to run that all subsequent "
+                                 "processes are also set to run.")
+
+    def _str2bool(self, v):
+        '''
+        Convenience function to convert strings to boolean values.
+        '''
+        val = v.lower()
+        if val not in ("yes", "true", "t", "1", "no", "false", "f", "0"):
+            raise ValueError('Boolean flag key not set to allowed value.')
+        else:
+            return v.lower() in ("yes", "true", "t", "1")
+
+    def read_metadata(self):
+        '''
+        Helper function to read in metadata and set various class attributes.
+        '''
+        config = ConfigParser()
+        with open(self.file_path) as f:
+            config.read_string('[config]\n' + f.read())
+        config = config['config']
+
+        for run_flag in ['include_perturb_auf', 'include_phot_like']:
+            if run_flag in config:
+                setattr(self, run_flag, self._str2bool(config[run_flag]))
+            else:
+                raise ValueError("Missing key {} from metadata file.".format(run_flag))
+
+        for run_flag in ['run_auf', 'run_group', 'run_cf', 'run_star']:
+            if run_flag in config:
+                setattr(self, run_flag, self._str2bool(config[run_flag]))
+            else:
+                raise ValueError("Missing key {} from metadata file.".format(run_flag))
--- macauff/tests/test_matching.py
+++ macauff/tests/test_matching.py
@@ -4,6 +4,7 @@ Tests for the "matching" module.
 '''
 
 import pytest
+import os
 
 from ..matching import CrossMatch
 
@@ -11,3 +12,18 @@ from ..matching import CrossMatch
 def test_crossmatch_input():
     with pytest.raises(FileNotFoundError):
         cm = CrossMatch('./file.txt')
+
+    cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata.txt'))
+    assert cm.run_auf is False
+    assert cm.run_group is False
+    assert cm.run_cf is True
+    assert cm.run_star is True
+
+    with pytest.raises(ValueError):
+        cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_missing_key.txt'))
+
+    with pytest.raises(ValueError):
+        cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_bad_type.txt'))
+
+    with pytest.raises(ValueError):
+        cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_bad_order.txt'))
