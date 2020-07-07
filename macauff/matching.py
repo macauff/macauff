b6de8f9786d3ca6df69fd2622c5ec71cf55bbf22
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-07-07 20:39:21 +0100

    Added metadata reading and tests for AUF region code, either through an evenly spaced rectangle grid or through individually specified coordinate tuples

--- macauff/matching.py
+++ macauff/matching.py
@@ -80,10 +80,51 @@ class CrossMatch():
         config = config['config']
 
         for check_flag in ['include_perturb_auf', 'include_phot_like', 'run_auf', 'run_group',
-                           'run_cf', 'run_star']:
+                           'run_cf', 'run_star', 'auf_region_type', 'auf_region_frame',
+                           'auf_region_points']:
             if check_flag not in config:
                 raise ValueError("Missing key {} from metadata file.".format(check_flag))
 
         for run_flag in ['include_perturb_auf', 'include_phot_like', 'run_auf', 'run_group',
                          'run_cf', 'run_star']:
             setattr(self, run_flag, self._str2bool(config[run_flag]))
+
+        art = config['auf_region_type'].lower()
+        if art == 'rectangle':
+            try:
+                a = config['auf_region_points'].split()
+                a = [float(point) for point in a]
+            except ValueError:
+                raise ValueError("auf_region_points should be 6 numbers separated "
+                                 "by spaces.")
+            if len(a) == 6:
+                if not a[2].is_integer() or not a[5].is_integer():
+                    raise ValueError("Number of steps between start and stop values for "
+                                     "auf_region_points should be integers.")
+                ax1_p = np.linspace(a[0], a[1], int(a[2]))
+                ax2_p = np.linspace(a[3], a[4], int(a[5]))
+                points = np.stack(np.meshgrid(ax1_p, ax2_p), -1).reshape(-1, 2)
+            else:
+                raise ValueError("auf_region_points should be 6 numbers separated "
+                                 "by spaces.")
+        elif art == 'points':
+            try:
+                a = config['auf_region_points'].replace('(', ')').split('), )')
+                # Remove the first ( and final ) that weren't split by "), (" -> "), )"
+                a[0] = a[0][1:]
+                a[-1] = a[-1][:-1]
+                b = [q.split(', ') for q in a]
+                points = np.array(b, dtype=float)
+            except ValueError:
+                raise ValueError("auf_region_points should be a list of '(a, b), (c, d)' tuples, "
+                                 "separated by a comma.")
+        else:
+            raise ValueError("auf_region_type should either be 'rectangle' or 'points'.")
+
+        self.auf_region_points = points
+
+        arf = config['auf_region_frame'].lower()
+        if arf == 'equatorial' or arf == 'galactic':
+            self.auf_region_frame = arf
+        else:
+            raise ValueError("auf_region_frame should either be 'equatorial' or 'galactic'.")
--- macauff/tests/test_matching.py
+++ macauff/tests/test_matching.py
@@ -5,6 +5,7 @@ Tests for the "matching" module.
 
 import pytest
 import os
+from numpy.testing import assert_almost_equal
 import numpy as np
 
 from ..matching import CrossMatch
@@ -20,14 +21,97 @@ def test_crossmatch_input():
     assert cm.run_cf is True
     assert cm.run_star is True
 
+    assert cm.auf_region_frame == 'equatorial'
+    assert_almost_equal(cm.auf_region_points,
+                        np.array([[131, -1], [132, -1], [133, -1], [134, -1],
+                                  [131, 0], [132, 0], [133, 0], [134, 0],
+                                  [131, 1], [132, 1], [133, 1], [134, 1]]))
+
+    # List of simple one line config file replacements for error message checking
     f = open(os.path.join(os.path.dirname(__file__), 'data/metadata.txt')).readlines()
-    for old_line, new_line in zip(['run_cf = yes', 'run_auf = no', 'run_auf = no'],
-                                  ['', 'run_auf = aye\n', 'run_auf = yes\n']):
-        miss_key_idx = np.where([old_line in line for line in f])[0][0]
+    for old_line, new_line in zip(['run_cf = yes', 'run_auf = no', 'run_auf = no',
+                                   'auf_region_type = rectangle', 'auf_region_type = rectangle',
+                                   'auf_region_points = 131 134 4 -1 1 3',
+                                   'auf_region_points = 131 134 4 -1 1 3',
+                                   'auf_region_frame = equatorial'],
+                                  ['', 'run_auf = aye\n', 'run_auf = yes\n',
+                                   '', 'auf_region_type = triangle\n',
+                                   'auf_region_points = 131 134 4 -1 1 a\n',
+                                   'auf_region_points = 131 134 4 -1 1\n',
+                                   'auf_region_frame = ecliptic\n']):
+        idx = np.where([old_line in line for line in f])[0][0]
         CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata.txt'),
-                                 miss_key_idx, new_line,
-                                 out_file=os.path.join(os.path.dirname(__file__),
-                                                       'data/metadata_.txt'))
+                                 idx, new_line, out_file=os.path.join(os.path.dirname(__file__),
+                                                                      'data/metadata_.txt'))
 
         with pytest.raises(ValueError):
             cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_.txt'))
+
+    # Check correct and incorrect auf_region_points when auf_region_type is 'points'
+    idx = np.where(['auf_region_type = rectangle' in line for line in f])[0][0]
+    CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata.txt'),
+                             idx, 'auf_region_type = points\n',
+                             out_file=os.path.join(os.path.dirname(__file__),
+                                                   'data/metadata_.txt'))
+
+    idx = np.where(['auf_region_points = 131 134 4 -1 1 3' in line for line in f])[0][0]
+    CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata_.txt'),
+                             idx, 'auf_region_points = (131, 0), (133, 0), (132, -1)\n',
+                             out_file=os.path.join(os.path.dirname(__file__),
+                                                   'data/metadata_2.txt'))
+
+    cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_2.txt'))
+    assert_almost_equal(cm.auf_region_points, np.array([[131, 0], [133, 0], [132, -1]]))
+
+    old_line = 'auf_region_points = 131 134 4 -1 1 3'
+    for new_line in ['auf_region_points = (131, 0), (131, )\n',
+                     'auf_region_points = (131, 0), (131, 1, 2)\n',
+                     'auf_region_points = (131, 0), (131, a)\n']:
+        idx = np.where([old_line in line for line in f])[0][0]
+        CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata_.txt'),
+                                 idx, new_line, out_file=os.path.join(os.path.dirname(__file__),
+                                                                      'data/metadata_2.txt'))
+
+        with pytest.raises(ValueError):
+            cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_2.txt'))
+
+    # Check galactic run is also fine
+    idx = np.where(['auf_region_frame = equatorial' in line for line in f])[0][0]
+    CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata.txt'),
+                             idx, 'auf_region_frame = galactic\n',
+                             out_file=os.path.join(os.path.dirname(__file__),
+                                                   'data/metadata_.txt'))
+
+    cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_.txt'))
+
+    assert cm.auf_region_frame == 'galactic'
+    assert_almost_equal(cm.auf_region_points,
+                        np.array([[131, -1], [132, -1], [133, -1], [134, -1],
+                                  [131, 0], [132, 0], [133, 0], [134, 0],
+                                  [131, 1], [132, 1], [133, 1], [134, 1]]))
+
+    # Check single-length point grids are fine
+    idx = np.where(['auf_region_points = 131 134 4 -1 1 3' in line for line in f])[0][0]
+    CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata.txt'),
+                             idx, 'auf_region_points = 131 131 1 0 0 1\n',
+                             out_file=os.path.join(os.path.dirname(__file__),
+                                                   'data/metadata_.txt'))
+
+    cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_.txt'))
+
+    assert_almost_equal(cm.auf_region_points, np.array([[131, 0]]))
+
+    idx = np.where(['auf_region_type = rectangle' in line for line in f])[0][0]
+    CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata.txt'),
+                             idx, 'auf_region_type = points\n',
+                             out_file=os.path.join(os.path.dirname(__file__),
+                                                   'data/metadata_.txt'))
+
+    idx = np.where(['auf_region_points = 131 134 4 -1 1 3' in line for line in f])[0][0]
+    CrossMatch._replace_line(cm, os.path.join(os.path.dirname(__file__), 'data/metadata_.txt'),
+                             idx, 'auf_region_points = (131, 0)\n',
+                             out_file=os.path.join(os.path.dirname(__file__),
+                                                   'data/metadata_2.txt'))
+
+    cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/metadata_2.txt'))
+    assert_almost_equal(cm.auf_region_points, np.array([[131, 0]]))
