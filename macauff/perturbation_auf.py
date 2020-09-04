b452a78dd89ca93bcf5dd0ad437d967db0fc4247
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-09-04 16:44:15 +0100

    Extended create_perturb_auf to save its dummy parameter arrays in the case where include_perturb_auf is False, and thus the cross-match process should be equivalent to not using this step at all.

--- macauff/perturbation_auf.py
+++ macauff/perturbation_auf.py
@@ -3,13 +3,15 @@
 This module provides the high-level framework for performing catalogue-catalogue cross-matches.
 '''
 
+import os
+import sys
 import numpy as np
 
 __all__ = ['create_perturb_auf']
 
 
 def create_perturb_auf(auf_folder, filters, auf_points, psf_fwhms, tri_download_flag, ax_lims,
-                       r, dr, rho, drho, which_cat):
+                       r, dr, rho, drho, which_cat, include_perturb_auf):
     """
     Function to perform the creation of the blended object perturbation component
     of the AUF.
@@ -39,4 +41,80 @@ def create_perturb_auf(auf_folder, filters, auf_points, psf_fwhms, tri_download_
         The fourier-space coordinates for Hankel transformations.
     drho : numpy.ndarray
         The spacings between ``rho`` elements.
+    which_cat : string
+        Indicator as to whether these perturbation AUFs are for catalogue "a"
+        or catalogue "b" within the cross-match process.
+    include_perturb_auf : boolean
+        ``True`` or ``False`` flag indicating whether perturbation component of the
+        AUF should be used or not within the cross-match process.
     """
+    print('Creating empirical crowding AUFs for catalogue "{}"...'.format(which_cat))
+    sys.stdout.flush()
+
+    # The number of simulated PSFs to create for statistical purposes
+    N_trials = 1000000
+    # Magnitude offsets corresponding to relative fluxes of perturbing sources; here
+    # dm of 2.5 is 10% relative flux and dm = 5 corresponds to 1% relative flux. Used
+    # to inform the fraction of simulations with a contaminant above these relative
+    # fluxes.
+    delta_mag_cuts = np.array([2.5, 5])
+
+    for i in range(len(auf_points)):
+        ax1, ax2 = auf_points[i]
+        ax_folder = '{}/{}/{}'.format(auf_folder, ax1, ax2)
+        if not os.path.exists(ax_folder):
+            os.makedirs(ax_folder, exist_ok=True)
+        if include_perturb_auf:
+            # TODO: download TRILEGAL simulation here
+            raise NotImplementedError("Perturbation AUF components are not currently"
+                                      "included in the cross-match process.")
+
+        for j in range(len(filters)):
+            filt = filters[j]
+
+            filt_folder = '{}/{}'.format(ax_folder, filt)
+            if not os.path.exists(filt_folder):
+                os.makedirs(filt_folder, exist_ok=True)
+
+            if include_perturb_auf:
+                # TODO: extend with create_single_auf
+                raise NotImplementedError("Perturbation AUF components are not currently"
+                                          "included in the cross-match process.")
+            else:
+                # Without the simulations to force local normalising density N or
+                # individual source brightness magnitudes, we can simply combine
+                # all data into a single "bin".
+                num_N_mag = 1
+                # In cases where we do not want to use the perturbation AUF component,
+                # we currently don't have separate functions, but instead set up dummy
+                # functions and variables to pass what mathematically amounts to
+                # "nothing" through the cross-match. Here we would use fortran
+                # subroutines to create the perturbation simulations, so we make
+                # f-ordered dummy parameters.
+                Frac = np.zeros((len(delta_mag_cuts), num_N_mag), float, order='F')
+                np.save('{}/{}/frac.npy'.format(filt_folder), Frac)
+                Flux = np.zeros(num_N_mag, float, order='F')
+                np.save('{}/{}/flux.npy'.format(filt_folder), Flux)
+                # Remember that r is bins, so the evaluations at bin middle are one
+                # shorter in length.
+                offset = np.zeros((len(r)-1, num_N_mag), float, order='F')
+                # Fix offsets such that the probability density function looks like
+                # a delta function, such that a two-dimensional circular coordinate
+                # integral would evaluate to one at every point, cf. ``cumuative``.
+                offset[0, :] = 1 / (2 * np.pi * (r[0] + dr[0]/2) * dr[0])
+                np.save('{}/{}/offset.npy'.format(filt_folder), offset)
+                # The cumulative integral of a delta function is always unity.
+                cumulative = np.ones((len(r)-1, num_N_mag), float, order='F')
+                np.save('{}/{}/cumulative.npy'.format(filt_folder), cumulative)
+                # The Hankel transform of a delta function is a flat line; this
+                # then preserves the convolution being multiplication in fourier
+                # space, as F(x) x 1 = F(x), similar to how f(x) * d(0) = f(x).
+                fourieroffset = np.ones((len(rho)-1, num_N_mag), float, order='F')
+                np.save('{}/{}/fourier.npy'.format(filt_folder), fourieroffset)
+                # Both normalising density and magnitude arrays can be proxied
+                # with a dummy parameter, as any minimisation of N-m distance
+                # must pick the single value anyway.
+                Narray = np.array([[1]], float)
+                np.save('{}/{}/N.npy'.format(filt_folder), Narray)
+                magarray = np.array([[1]], float)
+                np.save('{}/{}/mag.npy'.format(filt_folder), magarray)
