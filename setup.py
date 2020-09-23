1e4623b3ec7965816c770ae20c96bab8a00dbfc4
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-09-23 18:48:28 +0100

    Added perturbation_auf_fortran to setup.py, to be loaded automatically during package setup

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import find_packages
 from numpy.distutils.core import Extension, setup
 
-extension = []
+extension = [Extension(name='macauff.perturbation_auf_fortran',
+                       sources=['macauff/perturbation_auf_fortran.f90'], language='f90',
+                       extra_link_args=["-lgomp"],
+                       extra_f90_compile_args=["-Wall", "-Wextra", "-Werror", "-pedantic",
+                                               "-fbacktrace", "-O0", "-g", "-fcheck=all",
+                                               "-fopenmp"])]
 
 setup(name="macauff", packages=find_packages(),
       package_data={'macauff': ['tests/data/*']},
