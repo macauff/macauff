f22b3f9bd212434dde46f5f0f2408e0f0f1cd98d
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-10-30 14:43:22 +0000

    Added test for _load_cumulative_grid_cutouts that checks for padding = 0.

--- macauff/tests/test_group_sources.py
+++ macauff/tests/test_group_sources.py
@@ -11,7 +11,7 @@ import numpy as np
 from ..matching import CrossMatch
 from ..group_sources import make_island_groupings, _load_cumulative_grid_cutouts
 from ..misc_functions import create_cumulative_offsets_grid
-# from .group_sources_fortran import group_sources_fortran as gsf
+from ..group_sources_fortran import group_sources_fortran as gsf
 
 
 def test_load_cumulative_grid_cutouts():
@@ -21,7 +21,7 @@ def test_load_cumulative_grid_cutouts():
         a[i:i+10000, :] = 0
     a[0, :] = [50, 50, 0.1]
     a[123, :] = [48, 59, 0.5]
-    a[555, :] = [41, 43, 0.2]
+    a[555, :] = [39.98, 43, 0.2]
     a[1000, :] = [45, 45, 0.2]
 
     del a
@@ -50,22 +50,39 @@ def test_load_cumulative_grid_cutouts():
 
     a = np.lib.format.open_memmap('con_cat_astro.npy', mode='r', dtype=float, shape=(lena, 3))
     rect = np.array([40, 60, 40, 60])
-    padding = 0.1
 
-    a, b, c = _load_cumulative_grid_cutouts(a, rect, '.', '.', '.', padding)
-    assert np.all(a.shape == (4, 3))
-    assert np.all(a == np.array([[50, 50, 0.1], [48, 59, 0.5], [41, 43, 0.2], [45, 45, 0.2]]))
-    assert np.all(b.shape == (100, 1, 2, 2))
+    padding = 0.1
+    _a, _b, _c, _ = _load_cumulative_grid_cutouts(a, rect, '.', '.', '.', padding)
+    assert np.all(_a.shape == (4, 3))
+    assert np.all(_a == np.array([[50, 50, 0.1], [48, 59, 0.5], [39.98, 43, 0.2], [45, 45, 0.2]]))
+    assert np.all(_b.shape == (100, 1, 2, 2))
     b_guess = np.empty((100, 1, 2, 2), float)
     b_guess[:, 0, 0, 0] = 0 + 1 * 2 + 0 * 6
     b_guess[:, 0, 1, 0] = 0 + 2 * 2 + 0 * 6
     b_guess[:, 0, 0, 1] = 0 + 1 * 2 + 1 * 6
     b_guess[:, 0, 1, 1] = 0 + 2 * 2 + 1 * 6
-    assert np.all(b == b_guess)
-    assert np.all(c.shape == (3, 4))
+    assert np.all(_b == b_guess)
+    assert np.all(_c.shape == (3, 4))
     c_guess = np.empty((3, 4), int)
     c_guess[:, 0] = [0, 1, 1]
     c_guess[:, 1] = [0, 1, 1]
     c_guess[:, 2] = [0, 0, 0]
     c_guess[:, 3] = [0, 1, 1]
-    assert np.all(c == c_guess)
+    assert np.all(_c == c_guess)
+
+    # This should not return source #555 above, removing its potential reference index.
+    # Hence we only have one unique grid reference now.
+    padding = 0
+    _a, _b, _c, _ = _load_cumulative_grid_cutouts(a, rect, '.', '.', '.', padding)
+    assert np.all(_a.shape == (3, 3))
+    assert np.all(_a == np.array([[50, 50, 0.1], [48, 59, 0.5], [45, 45, 0.2]]))
+    assert np.all(_b.shape == (100, 1, 1, 1))
+    b_guess = np.empty((100, 1, 1, 1), float)
+    b_guess[:, 0, 0, 0] = 0 + 2 * 2 + 1 * 6
+    assert np.all(_b == b_guess)
+    assert np.all(_c.shape == (3, 3))
+    c_guess = np.empty((3, 3), int)
+    c_guess[:, 0] = [0, 0, 0]
+    c_guess[:, 1] = [0, 0, 0]
+    c_guess[:, 2] = [0, 0, 0]
+    assert np.all(_c == c_guess)
