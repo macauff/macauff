c0a247090b481eaeb659201f9615f1efde40d663
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-10-30 14:45:48 +0000

    Added __init__ check for group_sources_fortran.

--- macauff/__init__.py
+++ macauff/__init__.py
@@ -2,4 +2,5 @@ from .matching import *
 from .perturbation_auf import *
 from .perturbation_auf_fortran import *
 from .group_sources import *
+from .group_sources_fortran import *
 from .misc_functions import *
