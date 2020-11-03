1fd5c68ee02185b4b3f23cd9e096b00c4fca1ce5
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-11-03 18:00:18 +0000

    Added new miscellaneous functions to handle convenience functions to load grids or perform minor calculations.

--- macauff/misc_functions.py
+++ macauff/misc_functions.py
@@ -22,3 +22,32 @@ def create_cumulative_offsets_grid(auf_folder_path, auf_pointings, filt_names, r
                                      ax1, ax2, filt))
             cumulatoffgrids[:, :arraylengths[i, j], i, j] = cumulatoffgrid
     del arraylengths, longestNm, cumulatoffgrids
+
+
+def load_small_ref_ind_cumulative_grid(modrefind, auf_folder_path):
+    nmuniqueind, nmnewind = np.unique(modrefind[0, :], return_inverse=True)
+    filtuniqueind, filtnewind = np.unique(modrefind[1, :], return_inverse=True)
+    axuniqueind, axnewind = np.unique(modrefind[2, :], return_inverse=True)
+    cumulatoffgrid = np.asfortranarray(np.load('{}/cumulative_grid.npy'.format(
+                                       auf_folder_path), mmap_mode='r')[:, :, :,
+                                       axuniqueind][:, :, filtuniqueind, :][:,
+                                       nmuniqueind, :, :])
+    modrefindsmall = np.empty((3, modrefind.shape[1]), int, order='F')
+    del modrefind
+    modrefindsmall[0, :] = nmnewind
+    modrefindsmall[1, :] = filtnewind
+    modrefindsmall[2, :] = axnewind
+
+    return cumulatoffgrid, modrefindsmall
+
+
+def hav_dist_constant_lat(x_lon, x_lat, lon):
+    '''
+    Computes the Haversine formula in the limit that sky separation is only
+    determined by longitudinal separation (i.e., delta-lat is zero).
+    '''
+
+    dist = np.degrees(2 * np.arcsin(np.abs(np.cos(np.radians(x_lat)) *
+                                           np.sin(np.radians((x_lon - lon)/2)))))
+
+    return dist
--- macauff/tests/test_misc_functions.py
+++ macauff/tests/test_misc_functions.py
@@ -3,11 +3,15 @@
 Tests for the "misc_functions" module.
 '''
 
+import os
 import numpy as np
 from numpy.testing import assert_allclose
 import scipy.special
 
+from ..misc_functions import (create_cumulative_offsets_grid, load_small_ref_ind_cumulative_grid,
+                              hav_dist_constant_lat)
 from ..misc_functions_fortran import misc_functions_fortran as mff
+from ..perturbation_auf_fortran import perturbation_auf_fortran as paf
 
 
 def test_calc_j0():
@@ -16,3 +20,69 @@ def test_calc_j0():
     j0 = mff.calc_j0(r, rho)
     assert_allclose(j0, scipy.special.j0(2 * np.pi * r.reshape(-1, 1) * rho.reshape(1, -1)),
                     rtol=1e-5)
+
+
+def test_create_cumulative_offsets_grid():
+    a_len = np.array([[5, 10, 5], [15, 4, 8]], order='F')
+    np.save('arraylengths.npy', a_len)
+    auf_pointings = np.array([[10, 20], [50, 50], [100, -40]])
+    filt_names = ['W1', 'W2']
+    r = np.linspace(0, 5, 10)
+    for j in range(0, len(auf_pointings)):
+        ax1, ax2 = auf_pointings[j]
+        for i in range(0, len(filt_names)):
+            filt = filt_names[i]
+            os.makedirs('{}/{}/{}'.format(ax1, ax2, filt), exist_ok=True)
+            np.save('{}/{}/{}/cumulative.npy'.format(ax1, ax2, filt),
+                    (i + len(filt_names)*j)*np.ones((len(r[:-1]), a_len[i, j]), float))
+
+    create_cumulative_offsets_grid('.', auf_pointings, filt_names, r)
+    a = np.lib.format.open_memmap('{}/cumulative_grid.npy'.format(
+        '.'), mode='r', dtype=float, shape=(9, 15, 2, 3), fortran_order=True)
+    assert np.all(a.shape == (9, 15, 2, 3))
+    a_manual = -1*np.ones((9, 15, 2, 3), float, order='F')
+    for j in range(0, len(auf_pointings)):
+        for i in range(0, len(filt_names)):
+            a_manual[:, :a_len[i, j], i, j] = i + len(filt_names)*j
+    assert np.all(a == a_manual)
+
+
+def test_load_small_ref_ind_cumulative_grid():
+    a_len = np.array([[6, 10, 7], [15, 9, 8], [7, 10, 12], [8, 8, 11]], order='F')
+    auf_pointings = np.array([[10, 20], [50, 50], [100, -40]])
+    filt_names = ['W1', 'W2', 'W3', 'W4']
+    a = np.lib.format.open_memmap('{}/cumulative_grid.npy'.format(
+        '.'), mode='w+', dtype=float, shape=(9, 15, 4, 3), fortran_order=True)
+    for j in range(0, len(auf_pointings)):
+        for i in range(0, len(filt_names)):
+            a[:, :a_len[i, j], i, j] = (i*a_len[i, j] + a_len[i, j]*len(filt_names)*j +
+                                        np.arange(a_len[i, j]).reshape(1, -1))
+    del a
+    # Unique indices: 0, 1, 2, 5; 0, 3; 0, 1, 2
+    # These map to 0, 1, 2, 3; 0, 1; 0, 1, 2
+    modrefind = np.array([[0, 2, 0, 2, 1, 5], [0, 3, 3, 3, 3, 0], [0, 1, 2, 1, 2, 1]])
+    a, b = load_small_ref_ind_cumulative_grid(modrefind, '.')
+
+    new_small_modrefind = np.array([[0, 2, 0, 2, 1, 3], [0, 1, 1, 1, 1, 0], [0, 1, 2, 1, 2, 1]])
+    new_small_cumulativeoffgrid = np.empty((9, 4, 2, 3), float, order='F')
+    for j, j_old in enumerate([0, 1, 2]):
+        for i, i_old in enumerate([0, 3]):
+            for k, k_old in enumerate([0, 1, 2, 5]):
+                new_small_cumulativeoffgrid[:, k, i, j] = (
+                    k_old + i_old*a_len[i_old, j_old] + a_len[i_old, j_old]*len(filt_names)*j_old)
+
+    assert np.all(b.shape == (3, 6))
+    assert np.all(a.shape == (9, 4, 2, 3))
+    assert np.all(b == new_small_modrefind)
+    assert np.all(a == new_small_cumulativeoffgrid)
+
+
+def test_hav_dist_constant_lat():
+    lon1s = [0, 124.1, 65.34, 180, 324, 96.34]
+    lon2s = [359.1, 150.23, 165.3, 210, 10.3, 60.34]
+
+    for lat in [-86.4, -40.3, -10.1, 0, 15.5, 45.1, 73.14, 88.54]:
+        for lon1, lon2 in zip(lon1s, lon2s):
+            a = paf.haversine(lon1, lon2, lat, lat)
+            b = hav_dist_constant_lat(lon1, lat, lon2)
+            assert_allclose(a, b)
