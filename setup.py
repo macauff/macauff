694c1b39e0188a2976903a9b64e049a96d73ea0b
Tom J Wilson <t.j.wilson@exeter.ac.uk>
2020-11-18 16:44:52 +0000

    Changed setup.py to allow for building of fortran code which contains linked library code -- here the fortran file 'shared_library' is linked to all other functions, being callable from other functions only.

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import find_packages
 from numpy.distutils.core import Extension, setup
 
-extension = [Extension(name='macauff.perturbation_auf_fortran',
-                       sources=['macauff/perturbation_auf_fortran.f90'], language='f90',
-                       extra_link_args=["-lgomp"],
-                       extra_f90_compile_args=["-Wall", "-Wextra", "-Werror", "-pedantic",
-                                               "-fbacktrace", "-O0", "-g", "-fcheck=all",
-                                               "-fopenmp"]),
-             Extension(name='macauff.group_sources_fortran',
-                       sources=['macauff/group_sources_fortran.f90'], language='f90',
-                       extra_link_args=["-lgomp"],
-                       extra_f90_compile_args=["-Wall", "-Wextra", "-Werror", "-pedantic",
-                                               "-fbacktrace", "-O0", "-g", "-fcheck=all",
-                                               "-fopenmp"]),
-             Extension(name='macauff.misc_functions_fortran',
-                       sources=['macauff/misc_functions_fortran.f90'], language='f90',
-                       extra_link_args=["-lgomp"],
-                       extra_f90_compile_args=["-Wall", "-Wextra", "-Werror", "-pedantic",
-                                               "-fbacktrace", "-O0", "-g", "-fcheck=all",
-                                               "-fopenmp"])]
+names = ['perturbation_auf_fortran', 'group_sources_fortran', 'misc_functions_fortran']
+f90_args = ["-Wall", "-Wextra", "-Werror", "-pedantic", "-fbacktrace", "-O0", "-g", "-fcheck=all",
+            "-fopenmp"]
 
-setup(name="macauff", packages=find_packages(),
-      package_data={'macauff': ['tests/data/*']},
-      ext_modules=extension)
+extension = [Extension(name='macauff.{}'.format(name), sources=['macauff/{}.f90'.format(name)],
+                       language='f90', extra_link_args=["-lgomp"],
+                       extra_f90_compile_args=f90_args, libraries=['shared_library'])
+             for name in names]
+
+extension.extend([Extension(name='macauff.tests.test_misc_functions_fortran',
+                            sources=['macauff/tests/test_misc_functions_fortran.f90'],
+                            language='f90', extra_link_args=["-lgomp"],
+                            libraries=['shared_library'], extra_f90_compile_args=f90_args)])
+
+setup(name="macauff", packages=find_packages(), package_data={'macauff': ['tests/data/*']},
+      ext_modules=extension, libraries=[('shared_library',
+                                        dict(sources=['macauff/shared_library.f90'],
+                                         extra_f90_compile_args=f90_args,
+                                         extra_link_args=["-lgomp"], language="f90"))])
