d125281234bb1051cbe89916ddb8f8390fe5c5c2
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-12-14 15:12:56 +0000

    Fortran routine for calculating the contamination-included counterpart probability densities, given the various fourier-space representations of AUF components.

---
+++ macauff/counterpart_pairing_fortran.f90
@@ -0,0 +1,41 @@
+! Licensed under a 3-clause BSD style license - see LICENSE
+
+module counterpart_pairing_fortran
+! This module provides the Fortran code for the final act of cross-matching, in which
+! sources are paired within their respective islands, and counterparts are established.
+
+implicit none
+
+integer, parameter :: dp = kind(0.0d0)  ! double precision
+real(dp), parameter :: pi = 4.0_dp*atan(1.0_dp)
+
+contains
+
+subroutine contamination_match_probabilities(Fcc, Fcn, Fnc, Fnn, rho, drho, sep, Gcc, Gcn, Gnc, Gnn)
+    implicit none
+    double precision, intent(in) :: Fcc(:), Fcn(:), Fnc(:), Fnn(:), rho(:), drho(:), sep
+    double precision, intent(out) :: Gcc, Gcn, Gnc, Gnn
+    integer :: j
+    double precision :: j0, pi, z
+    Gcc = 0.0_dp
+    Gcn = 0.0_dp
+    Gnc = 0.0_dp
+    Gnn = 0.0_dp
+
+    do j = 1, size(rho)
+        z = rho(j)*sep*2.0_dp*pi
+        call jy01a(z, j0)
+        Gcc = Gcc + rho(j) * Fcc(j) * j0 * drho(j)
+        Gcn = Gcn + rho(j) * Fcn(j) * j0 * drho(j)
+        Gnc = Gnc + rho(j) * Fnc(j) * j0 * drho(j)
+        Gnn = Gnn + rho(j) * Fnn(j) * j0 * drho(j)
+    end do
+
+    Gcc = Gcc * 2.0_dp * pi
+    Gcn = Gcn * 2.0_dp * pi
+    Gnc = Gnc * 2.0_dp * pi
+    Gnn = Gnn * 2.0_dp * pi
+
+end subroutine contamination_match_probabilities
+
+end module counterpart_pairing_fortran
