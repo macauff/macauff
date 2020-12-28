c0868c244b457e505f4d91dd7c80bb47b1abf612
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2020-12-28 14:09:30 +0000

    Tests for the CrossMatch pair_sources call, and minor fixes to code to ensure all tests pass reliably.

--- macauff/tests/test_counterpart_pairing.py
+++ macauff/tests/test_counterpart_pairing.py
@@ -8,8 +8,10 @@ from numpy.testing import assert_allclose
 import numpy as np
 import pytest
 
+from ..matching import CrossMatch
 from ..counterpart_pairing import source_pairing, _individual_island_probability
 from ..counterpart_pairing_fortran import counterpart_pairing_fortran as cpf
+from .test_matching import _replace_line
 
 
 def test_calculate_contamination_probabilities():
@@ -35,8 +37,7 @@ class TestCounterpartPairing:
         seed = 8888
         rng = np.random.default_rng(seed)
         # Test will have three overlap islands: 2 a sources and 1 b source, and
-        # a single lonely a source. The two islands will have two counterparts
-        # and a single islands with three "field" sources.
+        # a single lonely a source. Also two islands each with one "field" object.
         self.a_astro = np.empty((7, 3), float)
         self.b_astro = np.empty((4, 3), float)
         self.a_sig, self.b_sig = 0.1, 0.08
@@ -73,7 +74,7 @@ class TestCounterpartPairing:
 
         self.rho = np.linspace(0, 100, 10000)
         self.drho = np.diff(self.rho)
-        self.n_fracs = 3
+        self.n_fracs = 2
 
         self.abinlengths = 2*np.ones((3, 1), int)
         self.bbinlengths = 2*np.ones((4, 1), int)
@@ -105,7 +106,7 @@ class TestCounterpartPairing:
         # We have to save various files for the source_pairing function to
         # pick up again.
         self.joint_folder_path = 'test_path'
-        self.a_cat_folder_path = ' gaia_folder'
+        self.a_cat_folder_path = 'gaia_folder'
         self.b_cat_folder_path = 'wise_folder'
         self.a_auf_folder_path = 'gaia_auf_folder'
         self.b_auf_folder_path = 'wise_auf_folder'
@@ -156,7 +157,7 @@ class TestCounterpartPairing:
         self.b_auf_pointings = np.array([[0.0, 0.0]])
         # However, source_pairing will create the flux and frac grids, this being
         # the first time we should have encountered them.
-        self.a_filt_names = np.array(['BP', 'G', 'RP'])
+        self.a_filt_names = np.array(['G_BP', 'G', 'G_RP'])
         for i in range(0, len(self.a_filt_names)):
             os.makedirs('{}/0.0/0.0/{}'.format(self.a_auf_folder_path, self.a_filt_names[i]),
                         exist_ok=True)
@@ -187,6 +188,8 @@ class TestCounterpartPairing:
         self.Nfa, self.Nfb = self.fa_priors[0, 0, 0], self.fb_priors[0, 0, 0]
 
     def test_individual_island_probability(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
         i = 0
         wrapper = [
             i, self.a_astro, self.a_photo, self.b_astro, self.b_photo, self.alist, self.alist_,
@@ -220,7 +223,52 @@ class TestCounterpartPairing:
         _prob = self.Nc*self.G*self.Nfa
         assert_allclose(prob, _prob, rtol=1e-5)
 
+    def test_individual_island_zero_probabilities(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
+        fa_array = np.zeros_like(self.fa_array)
+        fb_array = np.zeros_like(self.fb_array)
+        fa_priors = np.zeros_like(self.fa_priors)
+        fb_priors = np.zeros_like(self.fb_priors)
+        i = 0
+        wrapper = [
+            i, self.a_astro, self.a_photo, self.b_astro, self.b_photo, self.alist, self.alist_,
+            self.blist, self.blist_, self.agrplen, self.bgrplen, self.c_array, fa_array,
+            fb_array, self.c_priors, fa_priors, fb_priors, self.amagref,
+            self.bmagref, self.amodelrefinds, self.bmodelrefinds, self.abinsarray,
+            self.abinlengths, self.bbinsarray, self.bbinlengths, self.afrac_grids,
+            self.aflux_grids, self.bfrac_grids, self.bflux_grids, self.afourier_grids,
+            self.bfourier_grids, self.a_sky_inds, self.b_sky_inds, self.rho, self.drho,
+            self.n_fracs]
+        results = _individual_island_probability(wrapper)
+        (acrpts, bcrpts, acrptscontp, bcrptscontp, etacrpts, xicrpts, acrptflux, bcrptflux,
+         afield, bfield, prob, integral) = results
+
+        assert np.all(acrpts == np.array([0]))
+        assert np.all(bcrpts == np.array([1]))
+
+        c_array = np.zeros_like(self.c_array)
+        c_priors = np.zeros_like(self.c_priors)
+        wrapper = [
+            i, self.a_astro, self.a_photo, self.b_astro, self.b_photo, self.alist, self.alist_,
+            self.blist, self.blist_, self.agrplen, self.bgrplen, c_array, self.fa_array,
+            self.fb_array, c_priors, self.fa_priors, self.fb_priors, self.amagref,
+            self.bmagref, self.amodelrefinds, self.bmodelrefinds, self.abinsarray,
+            self.abinlengths, self.bbinsarray, self.bbinlengths, self.afrac_grids,
+            self.aflux_grids, self.bfrac_grids, self.bflux_grids, self.afourier_grids,
+            self.bfourier_grids, self.a_sky_inds, self.b_sky_inds, self.rho, self.drho,
+            self.n_fracs]
+        results = _individual_island_probability(wrapper)
+        (acrpts, bcrpts, acrptscontp, bcrptscontp, etacrpts, xicrpts, acrptflux, bcrptflux,
+         afield, bfield, prob, integral) = results
+
+        assert len(acrpts) == 0
+        assert len(bcrpts) == 0
+        assert_allclose(prob/integral, 1)
+
     def test_source_pairing(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
         mem_chunk_num, n_pool = 2, 2
         source_pairing(
             self.joint_folder_path, self.a_cat_folder_path, self.b_cat_folder_path,
@@ -270,6 +318,8 @@ class TestCounterpartPairing:
         assert prob_b_field[q] == 1
 
     def test_including_b_reject(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
         # Remove the third group, pretending it's rejected in the group stage.
         alist = self.alist[:, [0, 1, 3, 4]]
         blist = self.blist[:, [0, 1, 3, 4]]
@@ -278,6 +328,7 @@ class TestCounterpartPairing:
 
         a_reject = np.array([2, 5])
         b_reject = np.array([3])
+        os.system('rm -r {}/reject'.format(self.joint_folder_path))
         os.makedirs('{}/reject'.format(self.joint_folder_path), exist_ok=True)
         np.save('{}/reject/reject_a.npy'.format(self.joint_folder_path), a_reject)
         np.save('{}/reject/reject_b.npy'.format(self.joint_folder_path), b_reject)
@@ -315,7 +366,7 @@ class TestCounterpartPairing:
 
         prob_counterpart = np.load('{}/pairing/pc.npy'.format(self.joint_folder_path))
         self._calculate_prob_integral()
-        _integral = self.Nc*self.G*self.Nfa + self.Nc*self.G_wrong*self.Nfa +\
+        _integral = self.Nc*self.G*self.Nfa + self.Nc*self.G_wrong*self.Nfa + \
             self.Nfa*self.Nfa*self.Nfb
         _prob = self.Nc*self.G*self.Nfa
         norm_prob = _prob/_integral
@@ -336,6 +387,8 @@ class TestCounterpartPairing:
         assert prob_b_field[q] == 1
 
     def test_small_length_warnings(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
         # Here want to test that the number of recorded matches -- either
         # counterpart, field, or rejected -- is lower than the total length.
         # To achieve this we fake reject length arrays smaller than their
@@ -391,6 +444,8 @@ class TestCounterpartPairing:
         assert np.all([q not in b_field for q in [0, 1, 3]])
 
     def test_large_length_warnings(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
         # Here want to test that the number of recorded matches -- either
         # counterpart, field, or rejected -- is higher than the total length.
         # To achieve this we fake reject length arrays larger than their
@@ -443,3 +498,130 @@ class TestCounterpartPairing:
         b_field = np.load('{}/pairing/bf.npy'.format(self.joint_folder_path))
         assert np.all([q in b_field for q in [2]])
         assert np.all([q not in b_field for q in [0, 1, 3]])
+
+    def _setup_cross_match_parameters(self):
+        for ol, nl in zip(['cf_region_points = 131 134 4 -1 1 3', 'mem_chunk_num = 10'],
+                          ['cf_region_points = 131 131 1 0 0 1\n', 'mem_chunk_num = 2\n']):
+            f = open(os.path.join(os.path.dirname(__file__),
+                                  'data/crossmatch_params.txt')).readlines()
+            idx = np.where([ol in line for line in f])[0][0]
+            _replace_line(os.path.join(os.path.dirname(__file__), 'data/crossmatch_params.txt'),
+                          idx, nl, out_file=os.path.join(os.path.dirname(__file__),
+                          'data/crossmatch_params_.txt'))
+
+        ol, nl = 'auf_region_points = 131 134 4 -1 1 {}', 'auf_region_points = 0 0 1 0 0 1\n'
+        for file_name in ['cat_a_params', 'cat_b_params']:
+            _ol = ol.format('3' if '_a_' in file_name else '4')
+            f = open(os.path.join(os.path.dirname(__file__),
+                                  'data/{}.txt'.format(file_name))).readlines()
+            idx = np.where([_ol in line for line in f])[0][0]
+            _replace_line(os.path.join(os.path.dirname(__file__), 'data/{}.txt'.format(file_name)),
+                          idx, nl, out_file=os.path.join(os.path.dirname(__file__),
+                          'data/{}_.txt'.format(file_name)))
+
+    def test_pair_sources(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
+        os.system('rm -r {}/reject/*'.format(self.joint_folder_path))
+        np.save('{}/group/alist.npy'.format(self.joint_folder_path), self.alist)
+        np.save('{}/group/blist.npy'.format(self.joint_folder_path), self.blist)
+        np.save('{}/group/agrplen.npy'.format(self.joint_folder_path), self.agrplen)
+        np.save('{}/group/bgrplen.npy'.format(self.joint_folder_path), self.bgrplen)
+        # Same run as test_source_pairing, but called from CrossMatch rather than
+        # directly this time.
+        self._setup_cross_match_parameters()
+        self.cm = CrossMatch(os.path.join(os.path.dirname(__file__),
+                             'data/crossmatch_params_.txt'),
+                             os.path.join(os.path.dirname(__file__), 'data/cat_a_params_.txt'),
+                             os.path.join(os.path.dirname(__file__), 'data/cat_b_params_.txt'))
+        self.cm.delta_mag_cuts = np.array([2.5, 5])
+        self.files_per_pairing = 13
+        self.cm.pair_sources(self.files_per_pairing)
+
+        bflux = np.load('{}/pairing/bcontamflux.npy'.format(self.joint_folder_path))
+        assert np.all(bflux == np.zeros((3), float))
+
+        a_matches = np.load('{}/pairing/ac.npy'.format(self.joint_folder_path))
+        assert np.all([q in a_matches for q in [0, 1, 2]])
+        assert np.all([q not in a_matches for q in [3, 4, 5, 6]])
+
+        a_field = np.load('{}/pairing/af.npy'.format(self.joint_folder_path))
+        assert np.all([q in a_field for q in [3, 4, 5, 6]])
+        assert np.all([q not in a_field for q in [0, 1, 2]])
+
+        b_matches = np.load('{}/pairing/bc.npy'.format(self.joint_folder_path))
+        assert np.all([q in b_matches for q in [0, 1, 3]])
+        assert np.all([q not in b_matches for q in [2]])
+
+        b_field = np.load('{}/pairing/bf.npy'.format(self.joint_folder_path))
+        assert np.all([q in b_field for q in [2]])
+        assert np.all([q not in b_field for q in [0, 1, 3]])
+
+        prob_counterpart = np.load('{}/pairing/pc.npy'.format(self.joint_folder_path))
+        self._calculate_prob_integral()
+        _integral = self.Nc*self.G*self.Nfa + self.Nc*self.G_wrong*self.Nfa + \
+            self.Nfa*self.Nfa*self.Nfb
+        _prob = self.Nc*self.G*self.Nfa
+        norm_prob = _prob/_integral
+        q = np.where([a_matches == 0])[0][0]
+        assert_allclose(prob_counterpart[q], norm_prob, rtol=1e-5)
+        xicrpts = np.load('{}/pairing/xi.npy'.format(self.joint_folder_path))
+        assert_allclose(xicrpts[q], np.array([np.log10(self.G / self.fa_priors[0, 0, 0])]),
+                        rtol=1e-6)
+
+        prob_a_field = np.load('{}/pairing/pfa.npy'.format(self.joint_folder_path))
+        a_field = np.load('{}/pairing/af.npy'.format(self.joint_folder_path))
+        q = np.where(a_field == 6)[0][0]
+        assert prob_a_field[q] == 1
+
+        prob_b_field = np.load('{}/pairing/pfb.npy'.format(self.joint_folder_path))
+        b_field = np.load('{}/pairing/bf.npy'.format(self.joint_folder_path))
+        q = np.where(b_field == 2)[0][0]
+        assert prob_b_field[q] == 1
+
+    def test_pair_sources_incorrect_warning(self):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
+        self._setup_cross_match_parameters()
+        for kind in ['cf', 'source']:
+            ol, nl = 'run_{} = yes'.format(kind), 'run_{} = no\n'.format(kind)
+            f = open(os.path.join(os.path.dirname(__file__),
+                                  'data/crossmatch_params.txt')).readlines()
+            idx = np.where([ol in line for line in f])[0][0]
+            _replace_line(os.path.join(os.path.dirname(__file__),
+                          'data/crossmatch_params_.txt'), idx, nl)
+        self.cm = CrossMatch(os.path.join(os.path.dirname(__file__),
+                             'data/crossmatch_params_.txt'),
+                             os.path.join(os.path.dirname(__file__), 'data/cat_a_params_.txt'),
+                             os.path.join(os.path.dirname(__file__), 'data/cat_b_params_.txt'))
+        self.cm.delta_mag_cuts = np.array([2.5, 5])
+        self.files_per_pairing = 13
+        with pytest.warns(UserWarning, match="Incorrect number of counterpart pairing files."):
+            self.cm.pair_sources(self.files_per_pairing)
+
+    def test_pair_sources_load_existing(self, capsys):
+        os.system('rm -r {}/pairing'.format(self.joint_folder_path))
+        os.makedirs('{}/pairing'.format(self.joint_folder_path), exist_ok=True)
+        # Fake files_per_pairing sources at random.
+        self.files_per_pairing = 13
+        for i in range(0, self.files_per_pairing):
+            np.save('{}/pairing/array_{}.npy'.format(self.joint_folder_path, i+1),
+                    np.zeros((4, 3), float))
+
+        self._setup_cross_match_parameters()
+        for kind in ['cf', 'source']:
+            ol, nl = 'run_{} = yes'.format(kind), 'run_{} = no\n'.format(kind)
+            f = open(os.path.join(os.path.dirname(__file__),
+                                  'data/crossmatch_params.txt')).readlines()
+            idx = np.where([ol in line for line in f])[0][0]
+            _replace_line(os.path.join(os.path.dirname(__file__),
+                          'data/crossmatch_params_.txt'), idx, nl)
+        self.cm = CrossMatch(os.path.join(os.path.dirname(__file__),
+                             'data/crossmatch_params_.txt'),
+                             os.path.join(os.path.dirname(__file__), 'data/cat_a_params_.txt'),
+                             os.path.join(os.path.dirname(__file__), 'data/cat_b_params_.txt'))
+        self.cm.delta_mag_cuts = np.array([2.5, 5])
+        capsys.readouterr()
+        self.cm.pair_sources(self.files_per_pairing)
+        output = capsys.readouterr().out
+        assert 'Loading pre-assigned counterparts' in output
