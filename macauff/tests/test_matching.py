2f90b13aef17991a17591eebcbcb3c9ee0ccad97
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2021-02-11 13:45:10 +0000

    Updated CrossMatch to move int_fracs to be a general input, added default to example config file, and updated coverage tests.

--- macauff/tests/test_matching.py
+++ macauff/tests/test_matching.py
@@ -552,6 +552,35 @@ class TestInputs:
                                 os.path.join(os.path.dirname(__file__),
                                 'data/cat_b_params{}.txt'.format('_' if '_b_' in in_file else '')))
 
+    def test_int_fracs(self):
+        cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/crossmatch_params.txt'),
+                        os.path.join(os.path.dirname(__file__), 'data/cat_a_params.txt'),
+                        os.path.join(os.path.dirname(__file__), 'data/cat_b_params.txt'))
+        assert np.all(cm.int_fracs == np.array([0.63, 0.9, 0.99]))
+
+        # List of simple one line config file replacements for error message checking
+        in_file = 'crossmatch_params'
+        f = open(os.path.join(os.path.dirname(__file__),
+                              'data/{}.txt'.format(in_file))).readlines()
+        old_line = 'int_fracs = 0.63 0.9 0.99'
+        for new_line, match_text in zip(
+                ['', 'int_fracs = 0.63 0.9 word\n', 'int_fracs = 0.63 0.9\n'],
+                ['Missing key int_fracs', 'All elements of int_fracs should be',
+                 'int_fracs should contain.']):
+            idx = np.where([old_line in line for line in f])[0][0]
+            _replace_line(os.path.join(os.path.dirname(__file__),
+                          'data/{}.txt'.format(in_file)), idx, new_line, out_file=os.path.join(
+                          os.path.dirname(__file__), 'data/{}_.txt'.format(in_file)))
+
+            with pytest.raises(ValueError, match=match_text):
+                cm = CrossMatch(os.path.join(os.path.dirname(__file__),
+                                'data/crossmatch_params{}.txt'.format(
+                                '_' if 'h_p' in in_file else '')),
+                                os.path.join(os.path.dirname(__file__),
+                                'data/cat_a_params{}.txt'.format('_' if '_a_' in in_file else '')),
+                                os.path.join(os.path.dirname(__file__),
+                                'data/cat_b_params{}.txt'.format('_' if '_b_' in in_file else '')))
+
     def test_crossmatch_chunk_num(self):
         cm = CrossMatch(os.path.join(os.path.dirname(__file__), 'data/crossmatch_params.txt'),
                         os.path.join(os.path.dirname(__file__), 'data/cat_a_params.txt'),
