f6f3edca78a6cd094a96e9eb0abe147ab7b60c5e
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2021-02-11 10:01:41 +0000

    Added extra fortran file to setup.py's compiling, in preparation of extra photometric likelihood code.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages
 from numpy.distutils.core import Extension, setup
 
-names = ['group_sources_fortran', 'misc_functions_fortran', 'counterpart_pairing_fortran']
+names = ['group_sources_fortran', 'misc_functions_fortran', 'counterpart_pairing_fortran',
+         'photometric_likelihood_fortran']
 f90_args = ["-Wall", "-Wextra", "-Werror", "-pedantic", "-fbacktrace", "-O0", "-g", "-fcheck=all",
             "-fopenmp"]
 
