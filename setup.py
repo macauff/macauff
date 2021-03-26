d8a8d54785e73849be270e1bbb825dc387a1032b
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2021-03-26 12:46:24 +0000

    Updated package creation workflow: added check on github pull request merge to run the full, remote data included tests of TRILEGAL simulation; added extra compilation of .f90 to setup.py; and added astropy as a dependency to tox.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from numpy.distutils.core import Extension, setup
 
 names = ['group_sources_fortran', 'misc_functions_fortran', 'counterpart_pairing_fortran',
-         'photometric_likelihood_fortran']
+         'photometric_likelihood_fortran', 'perturbation_auf_fortran']
 f90_args = ["-Wall", "-Wextra", "-Werror", "-pedantic", "-fbacktrace", "-O0", "-g", "-fcheck=all",
             "-fopenmp"]
 
