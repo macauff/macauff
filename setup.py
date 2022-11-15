1462898743a7ead97382d627e6ec51f54cb7f22d
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2022-11-15 10:53:40 +0000

    Added flag to toggle optimisation of fortran between O3 and O0+debug flags.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages
 from numpy.distutils.core import Extension, setup
+import sys
 
 names = ['group_sources_fortran', 'misc_functions_fortran', 'counterpart_pairing_fortran',
          'photometric_likelihood_fortran', 'perturbation_auf_fortran']
-f90_args = ["-Wall", "-Wextra", "-Werror", "-pedantic", "-fbacktrace", "-O3", "-g", "-fcheck=all",
-            "-fopenmp"]
+
+if "--full-build" in sys.argv:
+    f90_args = ["-O3", "-fopenmp"]
+    sys.argv.remove("--full-build")
+else:
+    f90_args = ["-Wall", "-Wextra", "-Werror", "-pedantic", "-fbacktrace", "-O0", "-g",
+                "-fcheck=all", "-fopenmp"]
 
 extension = [Extension(name='macauff.{}'.format(name), sources=['macauff/{}.f90'.format(name)],
                        language='f90', extra_link_args=["-lgomp"],
