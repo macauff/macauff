147d401f5ac304c0444118c02ef3b58eeedf5158
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2023-12-06 22:12:11 -0500

    shared_library linting.

--- tests/macauff/test_shared_library.py
+++ tests/macauff/test_shared_library.py
@@ -6,6 +6,7 @@ Tests for the "shared_library" module.
 import numpy as np
 from numpy.testing import assert_allclose
 
+# pylint: disable-next=import-error,no-name-in-module
 from macauff.misc_functions_fortran import misc_functions_fortran as mff
 
 
