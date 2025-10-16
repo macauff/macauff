8249d2c1c2d54a6a3c0e7a2f8a746a7464397441
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2025-10-16 19:59:23 +0100

    Re-check linux for div-by-zero

--- src/macauff/perturbation_auf_fortran.f90
+++ src/macauff/perturbation_auf_fortran.f90
@@ -125,6 +125,8 @@ subroutine perturb_aufs(Narray, magarray, r, dr, rbins, j0s, mag_D, dmag_D, Ds, 
 
         maxk = max(5, int(10*maxval(dNs)))
 
+        write(*, *) dNs, ";", maxk
+
         call scatter_perturbers(dNs, dms, psfr, maxk, dmcut, psfsig, offsets, fraccontam, fluxcontam, dd_params, l_cut, ddms, &
                                 algorithm_type, lentrials, seed(:, j))
         call histogram1d_dp(offsets, rbins(1), rbins(size(rbins)), size(r), midr, hist)
